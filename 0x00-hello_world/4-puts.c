6fe51be6aae79f528e75740479a0bbf20ad812b7
Mohammed <mohamedalisadek27102004@gmail.com>
2023-09-07 17:03:06 -0700

    my commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Describtion: A C program that prints text using puts function
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
