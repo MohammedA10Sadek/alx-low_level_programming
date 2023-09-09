1d7c1061c18819636ccd2f930da9ff9d24476f89
Mohammed <mohamedalisadek27102004@gmail.com>
2023-09-08 19:29:43 -0700

    g

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Describtion: a program that print from zero to nine.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+}
