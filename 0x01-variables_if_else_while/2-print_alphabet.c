8870db62c2b99fbe153194b5446aebb2f4401f25
Mohammed <mohamedalisadek27102004@gmail.com>
2023-09-08 18:51:31 -0700

    k

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Describtion: a program that prints alphapitic characters in lower case
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'a'
+
+	while (ch <= 'z')
+	{
+		putchat(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
