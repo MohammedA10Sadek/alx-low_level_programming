8701f4363ae9d5ed5ac4c7c5bfaded5e5bfd663a
Mohammed <mohamedalisadek27102004@gmail.com>
2023-09-08 19:37:47 -0700

    k

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Describtion: a program that prints alphapitic characters in reverse
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= ')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+
+	return (0);
+}
