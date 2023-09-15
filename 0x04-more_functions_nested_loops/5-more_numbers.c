ca7293d592385f8087ea93a9dbd76bcc2e31eef0
Mohammed <mohamedalisadek27102004@gmail.com>
2023-09-14 19:06:09 -0700

    lkl

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0-14 10times
+ * putchar 3 times of usage
+ *
+ * Return: void
+*/
+
+void print_numbers(void)
+{
+	int num, row, count;
+
+	for (row = 1; row <= 10; row++)
+	{
+		for (count = 0; count <= 14; count++)
+		{
+			num = count;
+			if (count > 9)
+			{
+				_putchar(1 + 48);
+				num = count % 10;
+			}
+			_putchar(num + 48);
+		}
+		_putchar('\n');
+	}
+}
