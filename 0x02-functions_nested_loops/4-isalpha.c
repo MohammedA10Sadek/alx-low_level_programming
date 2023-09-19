3c3dc7320b44d9c7d704cb9481f4bc35cb8a164e
Mohammed <mohamedalisadek27102004@gmail.com>
2023-09-19 03:10:52 -0700

    kk

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,9 +2,19 @@
 
 /**
  * _isalpha - checkif charachter is alpha
- *
+ * @c: is the charcter that we want to check its condition
  * Describtion:  program prints lowercase alphabet
  *
  * Return: void
 */
+
+int _isalpha(int c);
+
+	int i;
+
+	if ((c <= 90 && c >= 65) || (c <= 122 && c >= 97))
+		return (1);
+	else
+		return (0);
+
 
